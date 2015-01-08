c254726d51f9024c767cdb82734eb69c8c8c4cbe
Andre Spindler <git@andre-spindler.de>
2015-01-08 15:41:43 +0100

    Set constraints for TYPO3 version and PHP version

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "div"
+# Extension Manager/Repository config file for ext "div".
 #
-# Auto generated 23-01-2008 13:25
+# Auto generated 08-01-2015 15:38
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Collection of static functions and peer to access tx_lib. Auto including and loading of objects. NG typo3.teams.extension-coordination.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -27,20 +27,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearcacheonload' => 0,
 	'lockType' => '',
-	'author' => 'Extension Coordination Team (ECT)',
-	'author_email' => 'elmar07@googlemail.com',
-	'author_company' => 'Extension Coordination Team',
+	'author' => 'Andre Sindler',
+	'author_email' => 'git@andre-spindler.de',
+	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.2.0-4.5.99',
+			'php' => '4.0.0-5.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"8964";s:10:"README.txt";s:4:"ee2d";s:16:"class.tx_div.php";s:4:"bfbc";s:22:"class.tx_div_alpha.php";s:4:"8f36";s:19:"class.tx_div_ff.php";s:4:"c1f6";s:12:"ext_icon.gif";s:4:"b4e6";s:14:"doc/manual.sxw";s:4:"100f";s:14:"doc/manual.txt";s:4:"2a5d";s:14:"doc/phpdoc.ini";s:4:"5e47";s:19:"doc/wizard_form.dat";s:4:"722e";s:20:"doc/wizard_form.html";s:4:"8626";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"bac8";s:10:"README.txt";s:4:"ee2d";s:16:"class.tx_div.php";s:4:"bfbc";s:22:"class.tx_div_alpha.php";s:4:"8f36";s:19:"class.tx_div_ff.php";s:4:"c1f6";s:12:"ext_icon.gif";s:4:"b4e6";s:14:"doc/manual.sxw";s:4:"100f";s:14:"doc/manual.txt";s:4:"2a5d";s:14:"doc/phpdoc.ini";s:4:"5e47";s:19:"doc/wizard_form.dat";s:4:"722e";s:20:"doc/wizard_form.html";s:4:"8626";}',
+	'suggests' => array(
+	),
 );
 
 ?>
