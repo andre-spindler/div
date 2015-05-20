aa7237169de156183390bd753e5ac72383d80ae0
Andre Spindler <git@andre-spindler.de>
2015-05-20 13:52:43 +0200

    added autoloader as compatibility layer when calling the non-namespaced classes.

---
+++ ext_autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+$extpath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('div');
+
+return array(
+    'tx_div' => $extpath . 'class.tx_div.php',
+    'tx_div_alpha' => $extpath . 'class.tx_div_alpha.php',
+    'tx_div_ff' => $extpath . 'class.tx_div_ff.php',
+
+);
